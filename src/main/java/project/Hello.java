2104581ba756b2afa8987fdd2e7266b1405ff0e0
Zaylet Feliciano <zayletfm@gmail.com>
2023-02-14 14:30:40 -0500

    Update: currently advanced to section five in the udemy course

--- src/main/java/project/Hello.java
+++ src/main/java/project/Hello.java
@@ -35,5 +35,28 @@ public class Hello {
         if (!isCar) {
             System.out.println("This is not supposed to happen");
         }
+
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;  //this example is to demonstrate the ternary operator, but is not proper code
+        //boolean isDomestic = (makeOfCar != "Volkswagen"); simpler way to write the code
+        if (isDomestic){
+            System.out.println("This car is domestic to our country");
+        }
+
+        String s = (isDomestic) ? "this car is domestic" : "This car is not domestic";
+        System.out.println(s);
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+        System.out.println("My values total = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("The remainder = " + theRemainder);
+        boolean isNotRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("is not remainder = " + isNotRemainder);
+        if (!isNotRemainder) {
+            System.out.println("Got some remainder");
+        }
+
     }
 }
