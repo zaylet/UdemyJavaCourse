e7ee382ce03ae2d028d2f66b5bff174f9b484bf8
Zaylet Feliciano <zayletfm@gmail.com>
2023-02-20 15:58:37 -0500

    methods

--- src/main/java/MethodsChallenge.java
+++ src/main/java/MethodsChallenge.java
@@ -34,5 +34,18 @@ public class MethodsChallenge {
         }
 
         return 4;
+
+        //another way of doing the same thing as above!
+        
+//        int position 4;
+//        if (playerScore >= 1000) {
+//            position = 1;
+//        } else if (playerScore >= 500) {
+//            position = 2;
+//        } else if (playerScore >= 100) {
+//            position = 3;
+//        }
+//
+//        return position;
     }
 }
